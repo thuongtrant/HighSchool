498dc2d9ec3ac64057c095f302f3c5cbd5dab88b
your name <t.t.thuong2004@gmail.com>
2023-07-15 16:12:28 +0700

    improve_slider

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function hidden()
 {
     ficon.style.right = "-200px";
 }
+
+document.getElementById('nextID').onclick = function(){
+    const widthItem = document.querySelector('.slider').offsetWidth;
+    document.getElementById('formlistID').scrollLeft += widthItem;
+}
+document.getElementById('prevID').onclick = function(){
+    const widthItem = document.querySelector('.slider').offsetWidth;
+    document.getElementById('formlistID').scrollLeft -= widthItem;
+}
