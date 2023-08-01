fcaa8050105a9630be198940f88090819d3215af
Thuong Tran <t.t.thuong2004@gmail.com>
2023-08-01 22:29:22 +0700

    add animation

---
+++ assets/js/scroll-animation.js
@@ -0,0 +1,38 @@
+/**
+ * Check element position whether display on sceen at given rate location
+ * 
+ * @param {jQuery} $element 
+ * @param {number} rate element.top/window.innerHeight rate
+ * @returns {boolean}
+ */
+function isLocatedAt($element , rate) {
+    const elementTop = $element.get(0).getBoundingClientRect().top 
+    const windowHeight = window.innerHeight
+    return elementTop <= windowHeight*rate
+}
+ 
+/**
+ * Set animation by class on located
+ * 
+ * @param {jQuery} $element 
+ * @param {string} animationClass given animation css class in css file
+ */
+function setAnimation($element, animationClass) {
+    $element.each((i, e) => {
+        const isLocated = isLocatedAt($(e), 0.9)
+        if (isLocated) {
+            $(e).addClass(animationClass)
+        } else {
+            $(e).removeClass(animationClass)
+        }
+    })
+   
+}
+
+function setAnimationCallback() {
+    setAnimation($('.back-color'), 'back-color__slide-out')
+    setAnimation($('.block-intro__1'), 'block-intro__slide-out')
+   
+}
+$(window).on('scroll', setAnimationCallback)
+$(window).on('load', setAnimationCallback)
