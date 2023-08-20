44889f14c6bf7d83a5d73f5a3fc254c8938e70ab
Thuong Tran <t.t.thuong2004@gmail.com>
2023-08-20 20:11:15 +0700

    fix button backtop for pages

--- assets/js/backtop.js
+++ assets/js/backtop.js
@@ -1,14 +1,26 @@
 $(document).ready(function(){
+    autoShowBackTopButton();
     $(window).scroll(function(){
         if($(this).scrollTop()) {
             $('#backtop').fadeIn();
         } else {
             $('#backtop').fadeOut();
         }
-    })
+    }, autoShowBackTopButton)
     $("#backtop").click(function(){
         $('html,body').animate({
             scrollTop : 0
         }, 800)
     })
 })
+
+function autoShowBackTopButton() {
+    let header = document.querySelector('.nav');
+    const headerTop = header.getBoundingClientRect().top;
+    if (headerTop < 0) {
+        $('#backtop').fadeIn();
+    }
+    else {
+        $('#backtop').fadeOut();
+    }
+}
